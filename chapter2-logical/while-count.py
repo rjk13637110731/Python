3b5ae40f7296cb0dce0e8daa1b99448303c2f601
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2022-12-20 09:17:05 +0800

    Create while-count.py

---
+++ chapter2-logical/while-count.py
@@ -0,0 +1,14 @@
+count = 0
+while count <= 5:
+    print(count)
+    count += 1
+
+count1 = 0
+while count1 <= 5:
+    print(count1 + 2)
+    count1 += 1
+
+count2 = 0
+while count2 <= 6:
+    print(count2)
+    count2 += 2
