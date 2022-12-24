b439e6ba03a8956fea2bc5dc1699d504aa1b5b40
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2022-12-24 16:06:17 +0800

    Create same-element-change.py

---
+++ chapter7-set/same-element-change.py
@@ -0,0 +1,12 @@
+list01 = [3, 80, 45, 5, 80, 1]
+result = False
+for r in range(len(list01) - 1):
+    for c in range(r + 1, len(list01)):
+        if list01[r] == list01[c]:
+            print(f"具有相同的元素：{list01[r]}")
+            result = True
+            break
+    if result:
+        break
+if result == False:
+    print("没有相同项")
