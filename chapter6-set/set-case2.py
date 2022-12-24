adbe8f677d0d61871927fbb0fe49b625f900b856
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2022-12-24 11:53:44 +0800

    Create set-case2.py

---
+++ chapter6-set/set-case2.py
@@ -0,0 +1,8 @@
+set01 = {"曹操", "刘备", "孙权"}
+set02 = {"曹操", "刘备", "张飞", "关羽"}
+print(set01 & set02)
+print(set01 - set02)
+print(set02 - set01)
+print("张飞" in set01)
+print(set01 ^ set02)
+print(set01 | set02)
