86ecfef148935f5d1863354d26370714d44f2208
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-06-05 10:25:14 +0800

    Create copy1.py

---
+++ BaiYueHeiYu/copy1.py
@@ -0,0 +1,15 @@
+team1 = [
+    {
+        'name': '乔丹',
+        'height': 198
+    },
+    {
+        'name': '姚明',
+        'height': 223
+    }
+]
+
+team2 = team1
+
+team2[0]['name']='麦迪'
+print(team1[0]['name'])
