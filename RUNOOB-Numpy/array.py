2b7af405b79af5f8c9e8a2a988a39c1ac3834785
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-08-30 13:54:20 +0800

    Create array.py

---
+++ RUNOOB-Numpy/array.py
@@ -0,0 +1,18 @@
+import numpy as np 
+a = np.array([1,2,3])  
+print (a)
+
+
+import numpy as np
+a = np.array([[1,2],[3,4]])
+print(a)
+
+
+import numpy as np
+a = np.array([1,2,3,4,5],ndmin=2)
+print(a)
+
+
+import numpy as np
+a = np.array([1,2,3],dtype=complex)
+print(a)
