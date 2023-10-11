d7a342a8cad494ae5e2fa9dab234e3899aad7de9
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-10-11 14:46:35 +0800

    Create insert-commit-rollback.py

---
+++ MySQL/insert-commit-rollback.py
@@ -0,0 +1,31 @@
+import pymysql
+
+
+def mysql_db():
+    con = pymysql.connect(
+        host='localhost',
+        port=3306,
+        database='runoob',
+        charset='utf8',
+        user='root',
+        password='123456'
+    )
+
+    try:
+        with con.cursor() as cursor:
+            sql = "insert into runoob_tbl (runoob_id, runoob_title,runoob_author,submission_date)values(7,'学习SQL','SQL_USER',now())"
+            cursor.execute(sql)
+
+            con.commit()
+            print("提交成功！")
+
+    except Exception as e:
+        con.rollback()
+        print("数据库异常\n",e)
+    finally:
+        con.close()
+
+
+if __name__ == '__main__':
+    mysql_db()
+    print('连接成功数据库！')
