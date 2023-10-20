b625cf843798513b7391ee8b693759dcda1e216b
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-10-20 10:41:00 +0800

    Create ab.py

---
+++ 武沛齐Python/day04/ab.py
@@ -0,0 +1,17 @@
+file_data = open("user_info.txt", mode="ab")
+while True:
+    user = input("请输入用户名：")
+    if user.upper() == "Q":
+        break
+    pwd = input("请输入密码：")
+    if pwd.upper() == "Q":
+        break
+
+    line = "{},{}\n".format(user, pwd)
+
+    # 写到计算机内存中了（缓冲区），还没写到文件中（硬盘中）
+    file_data.write(line.encode("utf-8"))
+
+    # 强制将内存中的数据写到硬盘中(文件中)
+    file_data.flush()
+file_data.close()
