52f066a405524cdb665773701bea68efea10f605
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-10-26 19:25:05 +0800

    Create MergedCell-value-all.py

---
+++ 武沛齐Python/day09/MergedCell-value-all.py
@@ -0,0 +1,49 @@
+import os
+from openpyxl import load_workbook
+from openpyxl.cell.cell import Cell, MergedCell
+
+# 1.获取work_book对象
+file_path = os.path.join("files", "output.xlsx")
+work_book_object = load_workbook(file_path)
+
+# 2.获取所有的sheet对象（内部包含很多数据）
+sheet_object_list = work_book_object.worksheets
+
+# 3.获取某一个sheet对象：方式1:
+sheet_object = sheet_object_list[1]
+
+
+# 4.获取当前sheet中所有被合并的单元格
+def get_merged_text(coordinate, sheet_object):
+    for item in sheet_object.merged_cells:
+        # print(item, type(item))
+        if coordinate in item:
+            # print("在")
+            # print(item.start_cell.value)
+            return item.start_cell.value
+
+
+# 5.如果是被合并的单元格的值等于 Cell的值
+for row in sheet_object.rows:
+    text_list = []
+    for cell in row:
+        # 判断cell是Cell对象则是正常单元格，如果是MergeCell对象则是合并单元格
+        if type(cell) == MergedCell:
+            # 获取单元格的内容
+            text = get_merged_text(cell.coordinate, sheet_object)
+            text_list.append(text)
+        elif type(cell) == Cell:
+            text_list.append(cell.value)
+        else:
+            text_list.append("不认识")
+    print(text_list)
+
+
+# 输出
+['用户信息', '用户信息', '备注']
+['学生姓名', '学生ID', None]
+['我害怕', '14789456546546', None]
+['我害怕', '14789456546545', None]
+['大白菜', '14346546546541', None]
+['ff', 'ff', 'ff']
+['ff', 'ff', 'ff']
